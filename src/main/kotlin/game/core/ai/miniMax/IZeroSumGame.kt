02e035ef9fdfbe1769bd3aa5f30e8f3194c82998
Josue Valenzuela <josue@kemik.gt>
2020-05-28 19:13:59 -0600

    Remove old minimax

--- src/main/kotlin/game/core/ai/miniMax/IZeroSumGame.kt
+++
@@ -1,9 +0,0 @@
-package game.core.ai.miniMax
-
-interface IZeroSumGame {
-    fun getScore(): Int
-    fun isOver(): Boolean
-    fun isMaximizing(): Boolean
-    fun getStates(): List<IZeroSumGame>
-    fun move(move: Int)
-}
--- src/main/kotlin/game/core/games/dotsAndBoxes/gameLogic/DotsAndBoxes.kt
+++ src/main/kotlin/game/core/games/dotsAndBoxes/gameLogic/DotsAndBoxes.kt
@@ -2,16 +2,28 @@ package game.core.games.dotsAndBoxes.gameLogic
 
 import game.core.games.dotsAndBoxes.*
 import game.core.games.dotsAndBoxes.models.MarkType
+import game.core.games.dotsAndBoxes.models.PlayerTurn
 import org.pmw.tinylog.Logger
 
 class DotsAndBoxes: IDotsAndBoxes {
 
+    companion object {
+        fun withEmptyBoard(size: Int) = DotsAndBoxes()
+                .also { it.setBoard(listOf(MutableList(size) { EMPTY_SPACE }, MutableList(size) { EMPTY_SPACE }), PlayerTurn.PLAYER_1) }
+    }
+
     private var horizontalLines = mutableListOf<Int>()
     private var verticalLines = mutableListOf<Int>()
     private var boardSize = 0
+    private var currentPlayer = PlayerTurn.PLAYER_1
+    private var repeatingTurn = false
 
-    override fun setBoard(boardRepresentation: List<List<Int>>) {
-        Logger.info("Setting up new board")
+    override fun getPlayerTurn() = currentPlayer
+    override fun isRepeatingTurn() = repeatingTurn
+
+    override fun setBoard(boardRepresentation: List<List<Int>>, playerTurnId: PlayerTurn) {
+        //Logger.info("Setting up new board")
+        currentPlayer = playerTurnId
         horizontalLines = boardRepresentation.component1().toMutableList()
         verticalLines = boardRepresentation.component2().toMutableList()
         boardSize = horizontalLines.size
@@ -23,6 +35,11 @@ class DotsAndBoxes: IDotsAndBoxes {
         MarkType.VERTICAL -> verticalLines.size
         MarkType.HORIZONTAL -> horizontalLines.size
     }
+
+    override fun isGameOver() = MarkType.values()
+        .map { getEmptyPositions(it) }
+        .flatten()
+        .isEmpty()
 
     override fun getEmptyPositions(type: MarkType): List<Int> {
 
@@ -80,26 +97,69 @@ class DotsAndBoxes: IDotsAndBoxes {
         return Pair(player1, player2)
     }
 
-    override fun markPosition(position: Int, type: MarkType): Pair<Int, Int>? {
-        Logger.info("Current score: ${getClosedBoxesCount()} for board ${getBoardRepresentation()}")
-        val playerScores = getCurrentScore()
-        Logger.info("Player1 score: ${playerScores.first} Player2 score: ${playerScores.second}")
+    override fun markPosition(position: Int, type: MarkType, player: PlayerTurn): Pair<Int, Int>? {
+        val beforeMarkScore = getClosedBoxesCount()
+        val mark = markPositionOnBoard(position, type, 0)
+        val afterMarkScore = getClosedBoxesCount()
+
+        if(beforeMarkScore < afterMarkScore) {
+            forceMark(position, type, getMarkValue(player, afterMarkScore -  beforeMarkScore))
+            repeatingTurn = true
+        }
+        return mark
+    }
+
+    private fun getMarkValue(player: PlayerTurn, pointDiff: Int): Int {
+        return when(player) {
+            PlayerTurn.PLAYER_1 -> if(pointDiff == 1) FILLED_P11 else if(pointDiff == 2) FILLED_P12 else 0
+            PlayerTurn.PLAYER_2 -> if(pointDiff == 1) FILLED_P21 else if(pointDiff == 2) FILLED_P22 else 0
+        }
+    }
+
+    private fun markPositionOnBoard(position: Int, type: MarkType, mark: Int): Pair<Int, Int>? {
         return when(type) {
             MarkType.HORIZONTAL -> {
                 if(horizontalLines[position] == EMPTY_SPACE) {
-                    horizontalLines[position] = 0
-                    return Pair(type.position,position)
+                    horizontalLines[position] = mark
+                    Pair(type.position,position)
                 } else {
                     null
                 }
             }
             MarkType.VERTICAL -> {
                 if(verticalLines[position] == EMPTY_SPACE) {
-                    verticalLines[position] = 0
-                    return Pair(type.position, position)
+                    verticalLines[position] = mark
+                    Pair(type.position, position)
                 } else {
                     null
                 }
+            }
+        }
+    }
+
+    private fun forceMark(position: Int, type: MarkType, mark: Int): Pair<Int, Int> {
+        return when(type) {
+            MarkType.HORIZONTAL -> {
+                horizontalLines[position] = mark
+                Pair(type.position,position)
+            }
+            MarkType.VERTICAL -> {
+                verticalLines[position] = mark
+                Pair(type.position, position)
+            }
+        }
+    }
+
+
+    override fun emptyPositions(position: Int, type: MarkType): Pair<Int, Int> {
+        return when(type) {
+            MarkType.HORIZONTAL -> {
+                horizontalLines[position] = EMPTY_SPACE
+                Pair(type.position,position)
+            }
+            MarkType.VERTICAL -> {
+                verticalLines[position] = EMPTY_SPACE
+                Pair(type.position,position)
             }
         }
     }
